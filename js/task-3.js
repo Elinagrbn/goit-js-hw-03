a8aa01c8cea543864ef2bf4c17056108b6e16b3c
Elina <elinagurbanova1@gmail.com>
2026-03-06 20:24:20 +0100

    js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function filterArray(numbers, value) {
   const result = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      result.push(number[i]);
+      result.push(numbers[i]);
     }
   }
   return result;
